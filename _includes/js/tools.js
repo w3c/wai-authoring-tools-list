8969189dc706cac8c7540411495dc976f68ee46c
Hidde de Vries <hidde@hiddedevries.nl>
2019-11-14 17:51:25 +0100

    Add first version of tabs
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- _includes/js/tools.js
+++ _includes/js/tools.js
@@ -104,3 +104,39 @@ if (options) {
     });
   });
 }
+
+const tabsets = document.querySelectorAll('[data-do-tabs]');
+
+if (tabsets) {
+  [].forEach.call(tabsets, tabset => {
+    
+    const tabs = tabset.querySelectorAll('[data-tab]');
+    const tabpanels = tabset.querySelectorAll('[data-tabpanel]');
+
+    if (tabs) {
+      [].forEach.call(tabs, tab => {
+        tab.addEventListener('click', e => {
+          const relatedPanel = document.getElementById(tab.getAttribute('href').substr(1));
+
+          [].forEach.call(tabpanels, tabpanel => {
+            tabpanel.setAttribute('hidden', 'true');
+          });
+
+          relatedPanel.removeAttribute('hidden');
+          relatedPanel.focus();
+
+          [].forEach.call(tabs, tab => {
+            tab.removeAttribute('data-tab-current', '');
+          });
+
+          tab.setAttribute('data-tab-current', '');
+
+          e.preventDefault();
+        });
+      });
+    }
+
+    tabs[0].setAttribute('data-tab-current', '');
+
+  });
+}
